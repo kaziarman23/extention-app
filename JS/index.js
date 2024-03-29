ffd4d3775489a07f726f01d3f845c04af45d07fc
kaziarman23 <kaziarman@proton.me>
2024-03-29 07:58:27 +0600

    app almost complite

---
+++ JS/index.js
@@ -0,0 +1,27 @@
+// git push : 2
+
+let myLeads = [];
+let input = document.querySelector(".inputEL");
+let inputBtn = document.querySelector(".inputBtn");
+let ulEL = document.querySelector(".ulEL");
+
+inputBtn.addEventListener("click", function () {
+    myLeads.push(input.value);
+    render();
+    input.value = "";
+});
+
+function render() {
+    let listItem = "";
+
+    for (let i = 0; i < myLeads.length; i++) {
+        listItem += `
+        <li>
+         <a target ='_blank' href = '${myLeads[i]}'>
+            ${myLeads[i]}
+         </a>  
+        </li>`;
+    }
+
+    ulEL.innerHTML = listItem;
+}
